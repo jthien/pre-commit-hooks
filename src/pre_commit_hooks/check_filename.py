beed868ed251bb38602adf2ab0bf1b47ed839265
Joerg Thienenkamp <joerg.thienenkamp@t-online.de>
2023-01-07 18:42:39 +0100

    Add check-filename

---
+++ src/pre_commit_hooks/check_filename.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import argparse
+from typing import Sequence
+
+
+def check_filename_prefix(filename: str, prefixes: list) -> int:
+    retv = 1
+    print("Check " + filename)
+    for prefix in prefixes:
+        print("Start with " + prefix)
+        if filename.startswith(prefix):
+            retv = 0
+    print("Result " + str(retv))
+    return retv
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*")
+    parser.add_argument(
+        "--allowed_prefix",
+        dest="allowed_prefix",
+        action="append",
+        default=[],
+    )
+
+    args = parser.parse_args(argv)
+
+    retv = 0
+
+    for filename in args.filenames:
+        retv |= check_filename_prefix(filename, args.allowed_prefix)
+
+    return retv
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
